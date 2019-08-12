6b4896556145101f0d1fb79867385876f070d7b2
Zeev Rotshtein <zeevro@gmail.com>
2019-08-13 02:56:43 +0300

    Add custom Application class

---
+++ app/src/main/java/com/zeevro/avionicz/MyApp.java
@@ -0,0 +1,13 @@
+package com.zeevro.avionicz;
+
+import android.app.Application;
+
+import net.danlew.android.joda.JodaTimeAndroid;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JodaTimeAndroid.init(this);
+    }
+}
